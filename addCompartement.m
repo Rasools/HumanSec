335ac7c1d1f24aa3ccf4ef2106d587fdeda8562d
rasools <rasools@chalmers.se>
2020-03-02 16:07:46 +0100

    Update

--- addCompartement.m
+++ addCompartement.m
@@ -20,5 +20,4 @@ function newModel = addCompartement(model,comps,compNames,compOutside)
   newModel=model;
   newModel.comps = vertcat(model.comps,comps);
   newModel.compNames = vertcat(model.compNames,compNames);
-%  newModel.compOutside = vertcat(model.compOutside,compOutside);
 end
