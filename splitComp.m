2f5b74b90ff13e56c1113bffa9fb4fa1db5a3dd3
rasools <rasools@chalmers.se>
2020-03-04 01:11:18 +0100

    'gm' and 'rm' added

--- splitComp.m
+++ splitComp.m
@@ -15,10 +15,10 @@ function [mets, comps]=splitComp(metComps)
   %
   %
   %
-  % Rasool Saghaleyni, 2018-02-16
+  % Rasool Saghaleyni, 2020-03-03
   for i=1:numel(metComps)
     a1 = char(metComps(i));
-    if isempty(strfind(a1,'pm')) == 1 && isempty(strfind(a1,'cv')) == 1 && isempty(strfind(a1,'sv')) == 1
+    if isempty(strfind(a1,'pm')) == 1 && isempty(strfind(a1,'cv')) == 1 && isempty(strfind(a1,'sv')) == 1 && isempty(strfind(a1,'gm')) && isempty(strfind(a1,'rm'))
       a2 = a1(1:end-3);
       a3 = a1(end-2:end);
     else
