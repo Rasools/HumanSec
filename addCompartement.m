b591d6246cd5d12ed29266863b456e4ec3745df4
rasools <rasools@chalmers.se>
2020-09-24 17:38:33 +0200

    fix
    prevent adding repetetive compartments

--- addCompartement.m
+++ addCompartement.m
@@ -16,8 +16,12 @@ function newModel = addCompartement(model,comps,compNames,compOutside)
 %
 %
 %
-% Rasool Saghaleyni, 2020-02-22
-  newModel=model;
-  newModel.comps = vertcat(model.comps,comps);
-  newModel.compNames = vertcat(model.compNames,compNames);
+% Rasool Saghaleyni,                              2020-02-22 
+% don't add repetitive compartments               2020-09-23
+  newComps = setdiff(comps, model.comps);
+  [~, b] = ismember(newComps, comps);
+  newCompNames = compNames(b);
+  newModel = model;
+  newModel.comps = vertcat(model.comps,newComps);
+  newModel.compNames = vertcat(model.compNames,newCompNames);
 end
